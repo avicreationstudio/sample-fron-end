d13328021e1ada2078ef59be8f0b58ac64d17d93
avi <145189684+avicreationstudio@users.noreply.github.com>
2024-05-24 11:38:44 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(""); 
+  useEffect(()=> {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+    
+    fetch("https://adsfa-mu.vercel.app/", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        setValue(result);
+      })
+      .catch((error) => console.error(error));
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
         my name is avinash
+        <p>
+          { value }
+        </p>
       </header>
     </div>
   );
